cfa0c5dda20cd9876573dac1a65ce380c2eada04
Hassan Ali <hassan.ali@fizz.io>
2019-05-27 13:22:59 +0500

    replace Mqtt.net with M2Mqtt for .net 3.5 support

--- Assets/FizzClient/Scripts/Chat/Impl/FizzMQTTConnection.cs
+++ Assets/FizzClient/Scripts/Chat/Impl/FizzMQTTConnection.cs
@@ -1,160 +1,171 @@
-﻿using System;
+﻿using Fizz.Common;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
-using MQTTnet;
-using MQTTnet.Client;
-using Fizz.Common;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace Fizz.Chat.Impl
 {
-    public interface IFizzMqttConnection
+    public class FizzMqttConnectException : Exception
     {
-        int Id { get; }
-        Action<int, object, MqttClientConnectedEventArgs> Connected { set; get; }
-        Action<int, object, MqttClientDisconnectedEventArgs> Disconnected { set; get; }
-        Action<int, object, MqttApplicationMessageReceivedEventArgs> MessageReceived { set; get; }
+        public byte Code { get; private set; }
 
-        Task ConnectAsync();
-        Task DisconnectAsync();
+        public FizzMqttConnectException (byte code, string message) : base (message)
+        {
+            Code = code;
+        }
     }
 
-    public class FizzMQTTConnection: IFizzMqttConnection
+    public class FizzMqttAuthException : FizzMqttConnectException
     {
-        private static readonly FizzException ERROR_INVALID_CLIENT_ID = new FizzException(FizzError.ERROR_BAD_ARGUMENT, "invalid_client_id");
-        private static readonly FizzException ERROR_INVALID_USERNAME = new FizzException(FizzError.ERROR_BAD_ARGUMENT, "invalid_username");
-        private static readonly FizzException ERROR_INVALID_PASSWORD = new FizzException(FizzError.ERROR_BAD_ARGUMENT, "invalid_password");
-        private static readonly FizzException ERROR_INVALID_DISPATCHER = new FizzException(FizzError.ERROR_BAD_ARGUMENT, "invalid_dispatcher");
+        public FizzMqttAuthException () : base (5, "invalid_credentials")
+        { }
+    }
+
+    public class FizzMqttDisconnectedArgs
+    {
+        public bool ClientWasConnected { get; private set; }
+        public Exception Exception { get; private set; }
+
+        public FizzMqttDisconnectedArgs (bool clientWasConnected, Exception ex)
+        {
+            ClientWasConnected = clientWasConnected;
+            Exception = ex;
+        }
+    }
+
+    public interface IFizzMqttConnection
+    {
+        Action<object, bool> Connected { set; get; }
+        Action<object, FizzMqttDisconnectedArgs> Disconnected { set; get; }
+        Action<object, byte[]> MessageReceived { set; get; }
+
+        void ConnectAsync ();
+        void DisconnectAsync ();
+    }
+
+    public class FizzMQTTConnection : IFizzMqttConnection
+    {
+        private static readonly FizzException ERROR_INVALID_CLIENT_ID = new FizzException (FizzError.ERROR_BAD_ARGUMENT, "invalid_client_id");
+        private static readonly FizzException ERROR_INVALID_USERNAME = new FizzException (FizzError.ERROR_BAD_ARGUMENT, "invalid_username");
+        private static readonly FizzException ERROR_INVALID_PASSWORD = new FizzException (FizzError.ERROR_BAD_ARGUMENT, "invalid_password");
+        private static readonly FizzException ERROR_INVALID_DISPATCHER = new FizzException (FizzError.ERROR_BAD_ARGUMENT, "invalid_dispatcher");
 
         // TODO: make this exponential backoff
-        private static int RETRY_DELAY_MS = 10*1000;
+        private static int RETRY_DELAY_MS = 10 * 1000;
 
-        private static int nextId = 0;
-
-        private readonly object synclock = new object();
-        private readonly int _id;
+        private MqttClient _client;
+        private readonly string _username;
+        private readonly string _password;
         private readonly string _clientId;
-        private readonly IMqttClientOptions _options;
         private readonly bool _retry;
-        private readonly IFizzActionDispatcher _dispatcher;
-        private IMqttClient _client;
+        private readonly bool _cleanSession;
         private bool _manualDisconnect = false;
+        private readonly IFizzActionDispatcher _dispatcher;
 
         // TODO: make this thread safe using Interlocked.Add/Interlocked.Remove
-        public Action<int, object, MqttClientConnectedEventArgs> Connected { set; get; }
-        public Action<int, object, MqttClientDisconnectedEventArgs> Disconnected { get; set; }
-        public Action<int, object, MqttApplicationMessageReceivedEventArgs> MessageReceived { get; set; }
+        public Action<object, bool> Connected { set; get; }
+        public Action<object, FizzMqttDisconnectedArgs> Disconnected { set; get; }
+        public Action<object, byte[]> MessageReceived { set; get; }
 
-        public FizzMQTTConnection(string username,
+        public FizzMQTTConnection (string username,
                                   string password,
-                                  string clientId, 
-                                  bool retry, 
+                                  string clientId,
+                                  bool retry,
                                   bool cleanSession,
                                   IFizzActionDispatcher dispatcher)
         {
-            if (string.IsNullOrEmpty(clientId))
+            if (string.IsNullOrEmpty (clientId))
             {
                 throw ERROR_INVALID_CLIENT_ID;
             }
-            if (string.IsNullOrEmpty(username))
+            if (string.IsNullOrEmpty (username))
             {
                 throw ERROR_INVALID_USERNAME;
             }
-            if (string.IsNullOrEmpty(password))
+            if (string.IsNullOrEmpty (password))
             {
                 throw ERROR_INVALID_PASSWORD;
             }
             if (dispatcher == null)
             {
-                throw ERROR_INVALID_DISPATCHER; 
+                throw ERROR_INVALID_DISPATCHER;
             }
 
             _clientId = clientId;
-            _id = Interlocked.Increment(ref nextId);
-
-            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-                .WithClientId(_clientId)
-                .WithCredentials(username, password)
-                .WithTcpServer(FizzConfig.MQTT_HOST_ENDPOINT)
-                .WithCleanSession(cleanSession);
-
-            if (FizzConfig.MQTT_USE_TLS) {
-                builder.WithTls();
-            }
-
-            _options = builder.Build();
+            _username = username;
+            _password = password;
             _retry = retry;
+            _cleanSession = cleanSession;
             _dispatcher = dispatcher;
+
+            _client = new MqttClient (
+                            FizzConfig.MQTT_HOST_ENDPOINT,
+                            FizzConfig.MQTT_USE_TLS ? 8883 : 1883,
+                            FizzConfig.MQTT_USE_TLS,
+                            null, null,
+                            FizzConfig.MQTT_USE_TLS ? MqttSslProtocols.SSLv3 : MqttSslProtocols.None
+                        );
+
+            _client.ConnectionClosed += (sender, args) =>
+            {
+                OnDisconnected (true, null);
+            };
+
+            _client.MqttMsgPublishReceived += OnMqttMessageReceived;
         }
 
-        public int Id
+        public void ConnectAsync ()
         {
-            get
+            ThreadPool.QueueUserWorkItem (payload =>
             {
-                return _id;
-            }
-        }
-
-        public Task ConnectAsync()
-        {
-            lock (synclock)
-            {
-                if (_client != null)
+                try
                 {
-                    return Task.CompletedTask;
+                    byte ret = _client.Connect (_clientId, _username, _password, _cleanSession, 60);
+                    if (ret == 0)
+                    {
+                        if (Connected != null)
+                        {
+                            _dispatcher.Post (() => Connected.Invoke (this, _client.SessionPresent));
+                        }
+                    }
+                    else
+                    {
+                        switch (ret)
+                        {
+                            case 5:
+                                OnDisconnected (false, new FizzMqttAuthException ());
+                                break;
+                            default:
+                                OnDisconnected (false, new FizzMqttConnectException (ret, "connect_failed"));
+                                break;
+                        }
+                    }
                 }
-
-                _client = new MQTTnet.MqttFactory().CreateMqttClient();
-                _client.Connected += OnConnected;
-                _client.Disconnected += OnDisconnected;
-                _client.ApplicationMessageReceived += OnMessageReceieved;
-
-                var connected = new TaskCompletionSource<object>();
-                _client.ConnectAsync(_options)
-                       .ContinueWith(task => DispatchTask<object>(task, connected));
-                return connected.Task;
-            }
-        }
-
-        public Task DisconnectAsync()
-        {
-            lock (synclock)
-            {
-                if (_client == null)
+                catch (Exception ex)
                 {
-                    return Task.CompletedTask;
+                    OnDisconnected (false, ex);
                 }
-
-                var client = _client;
-                _client = null;
-                _manualDisconnect = true;
-
-                var disconnected = new TaskCompletionSource<object>();
-                return client.DisconnectAsync()
-                             .ContinueWith(task => DispatchTask<object>(task, disconnected));
-            }
+            });
         }
 
-        private void OnConnected(object sender, MqttClientConnectedEventArgs args)
+        public void DisconnectAsync ()
         {
-            if (Connected != null)
+            if (_client == null || !_client.IsConnected)
             {
-                _dispatcher.Post(() => Connected.Invoke(_id, sender, args));
+                return;
             }
+
+            _manualDisconnect = true;
+
+            _client.Disconnect ();
         }
 
-        private void OnMessageReceieved(object sender, MqttApplicationMessageReceivedEventArgs args)
-        {
-            if (MessageReceived != null)
-            {
-                _dispatcher.Post(() => MessageReceived.Invoke(_id, sender, args));
-            }
-        }
-
-        private void OnDisconnected(object sender, MqttClientDisconnectedEventArgs args)
+        private void OnDisconnected (bool clientConnected, Exception ex)
         {
             if (Disconnected != null)
             {
-                _dispatcher.Post(() => Disconnected.Invoke(_id, sender, args));
+                _dispatcher.Post (() => Disconnected.Invoke (this, new FizzMqttDisconnectedArgs (false, ex)));
             }
 
             if (_manualDisconnect)
@@ -162,35 +173,33 @@ namespace Fizz.Chat.Impl
                 _manualDisconnect = false;
                 return;
             }
+
             if (!_retry)
             {
                 return;
             }
 
-            _dispatcher.Delay(RETRY_DELAY_MS, () =>
+            _dispatcher.Delay (RETRY_DELAY_MS, () =>
             {
                 try
                 {
-					if (_client != null) {
-                    	_client.ConnectAsync(_options);
-					}
+                    if (_client != null)
+                    {
+                        ConnectAsync ();
+                    }
                 }
                 catch
                 {
-                    FizzLogger.E("Unable to reconnect to Fizz event service.");
+                    FizzLogger.E ("Unable to reconnect to Fizz event service.");
                 }
             });
         }
 
-        private void DispatchTask<T>(Task source, TaskCompletionSource<T> dest) where T: class
+        private void OnMqttMessageReceived (object sender, MqttMsgPublishEventArgs msg)
         {
-            if (source.Exception != null)
+            if (MessageReceived != null)
             {
-                _dispatcher.Post(() => dest.SetException(source.Exception));
-            }
-            else
-            {
-                _dispatcher.Post(() => dest.SetResult(null));
+                _dispatcher.Post (() => MessageReceived.Invoke (this, msg.Message));
             }
         }
     }
