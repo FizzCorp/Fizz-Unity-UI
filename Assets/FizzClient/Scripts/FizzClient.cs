6c23bf1bd16aa400dcc37a6005974381e3240857
Hassan Ali <hassan.ali@fizz.io>
2019-10-07 05:01:52 -0700

    version v1.5.5

--- Assets/FizzClient/Scripts/FizzClient.cs
+++ Assets/FizzClient/Scripts/FizzClient.cs
@@ -118,7 +118,7 @@ namespace Fizz
 
         public FizzClientState State { get; private set; }
 
-        public string Version { get { return "v1.5.4"; } }
+        public string Version { get { return "v1.5.5"; } }
 
         public void Close(Action<FizzException> callback)
         {
