43986e1346fd1bc9961161f20ec750ac633f0165
alisafdarct <ali.safdar@carameltec.com>
2021-02-23 18:00:18 +0500

    [asmdef] version bump to v1.6.4

--- Assets/FizzClient/Scripts/FizzClient.cs
+++ Assets/FizzClient/Scripts/FizzClient.cs
@@ -118,7 +118,7 @@ namespace Fizz
 
         public FizzClientState State { get; private set; }
 
-        public string Version { get { return "v1.6.3"; } }
+        public string Version { get { return "v1.6.4"; } }
 
         public void Close(Action<FizzException> callback)
         {
