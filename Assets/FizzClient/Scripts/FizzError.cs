e9301b8fd677abd4a4bf419bd243c612d4d49898
alisafdarct <ali.safdar@carameltec.com>
2021-04-19 12:46:40 +0500

    [connect-flow] updated FizzErrors

--- Assets/FizzClient/Scripts/FizzError.cs
+++ Assets/FizzClient/Scripts/FizzError.cs
@@ -2,9 +2,9 @@
 {
     public class FizzError
     {
-        public static readonly int ERROR_BAD_ARGUMENT = 0xF122400;
-        public static readonly int ERROR_REQUEST_FAILED = 0xF122401;
-        public static readonly int ERROR_INVALID_REQUEST = 0xF122402;
-        public static readonly int ERROR_AUTH_FAILED = 0xF122403;
+        public const int ERROR_BAD_ARGUMENT = 0xF122400;
+        public const int ERROR_REQUEST_FAILED = 0xF122401;
+        public const int ERROR_INVALID_REQUEST = 0xF122402;
+        public const int ERROR_AUTH_FAILED = 0xF122403;
     }
 }
